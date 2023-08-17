4fd5842d9c23f8ac4a59502a464dcd78fed236bf
CaamVilvactDJavu <caam.vilvact11@gmail.com>
2023-08-17 23:35:36 +0700

    Get Table is success

--- backend/scraper.py
+++ backend/scraper.py
@@ -1,31 +1,65 @@
-from flask import Flask, render_template_string
-import requests
+from flask import Flask, jsonify
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from bs4 import BeautifulSoup
 from flask_cors import CORS
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 
-app = Flask(__name__, static_folder='public', static_url_path='')
-CORS(app)  # This will allow all origins to access your Flask API
+
+app = Flask(__name__)
+CORS(app)
 
 
-@app.route('/sensors')
+@app.route('/sensors', methods=['GET', 'POST'])
 def sensors():
-    url = "http://202.90.198.40/sismon-wrs/web/detail_slmon2/AAFM"
-    response = requests.get(url)
-    soup = BeautifulSoup(response.content, 'html.parser')
+    url = "http://202.90.198.40/sismon-wrs/web/slmon2/"
 
-    rows = soup.select('table tr')
-    table_data = [[cell.get_text(strip=True)
-                   for cell in row.select('td')] for row in rows]
+    # Setup Selenium Chrome options
+    chrome_options = Options()
+    # Run Chrome in headless mode (no GUI)
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--disable-gpu")
+    chrome_options.add_argument("--no-sandbox")
 
-    html_table = '<table>'
-    for row in table_data:
-        html_table += "<tr>"
-        for cell in row:
-            html_table += f"<td>{cell}</td>"
-        html_table += "</tr>"
-    html_table += "</table>"
+    with webdriver.Chrome(options=chrome_options) as driver:
+        driver.get(url)
 
-    return render_template_string(html_table)
+        WebDriverWait(driver, 10).until_not(
+            EC.text_to_be_present_in_element(
+                (By.XPATH, "//table[@id='test']//td[contains(text(), 'Loading...')]"), "Loading...")
+        )
+
+        # Extract page content
+        page_content = driver.page_source
+
+    soup = BeautifulSoup(page_content, 'html.parser')
+
+    # Extract the table
+    table = soup.find("table", {"id": "test"})
+    if not table:
+        return jsonify({"error": "Table not found"}), 404
+
+    # Extract table headers
+    headers = [header.text for header in table.find("thead").find_all("th")]
+
+    # Extract rows
+    tbody = table.find("tbody")
+    if not tbody:
+        return jsonify({"error": "Table body not found"}), 404
+
+    rows = tbody.find_all("tr")
+    table_data = []
+
+    for row in rows:
+        columns = row.find_all("td")
+        row_data = {}
+        for header, column in zip(headers, columns):
+            row_data[header] = column.text.strip()
+        table_data.append(row_data)
+
+    return jsonify(table_data)
 
 
 if __name__ == "__main__":
--- frontend/src/Sensor.jsx
+++ frontend/src/Sensor.jsx
@@ -3,17 +3,18 @@ import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 
 function Sensor() {
-    const [table, setTable] = useState("<p>Loading...</p>");
+    const [rows, setRows] = useState([]);
+    const [error, setError] = useState(null);  // New error state
 
     useEffect(() => {
         async function fetchData() {
             try {
                 const response = await fetch("http://localhost:3000/sensors");
-                const result = await response.text();
-                setTable(result);
+                const result = await response.json();
+                setRows(result);
             } catch (error) {
                 console.error("Error fetching data:", error);
-                setTable("<p>Error loading data.</p>");
+                setError("Error loading data.");  // Set error here
             }
         }
         fetchData();
@@ -22,9 +23,32 @@ function Sensor() {
     return (
         <>
             <Navbar />
-            <h1 className="text-4xl mb-6 font-bold text-center">Sensors</h1>
+            <h0 className="text-4xl mb-6 font-bold text-center">Sensors</h0>
             <div className="min-h-screen overflow-x-auto">
-                <div className="table w-full" dangerouslySetInnerHTML={{ __html: table }}></div>
+                {error ? (  // First check for error
+                    <p>{error}</p>
+                ) : rows.length === 0 ? (
+                    <p>Loading...</p>
+                ) : (
+                    <table className="table w-full">
+                        <thead>
+                            <tr>
+                                {Object.keys(rows[0]).map((header) => (
+                                    <th key={header}>{header}</th>
+                                ))}
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {rows.map((row, rowIndex) => (
+                                <tr key={rowIndex}>
+                                    {Object.values(row).map((value, valueIndex) => (
+                                        <td key={valueIndex}>{value}</td>
+                                    ))}
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                )}
             </div>
             <Footer />
         </>
@@ -32,4 +56,3 @@ function Sensor() {
 }
 
 export default Sensor;
-
