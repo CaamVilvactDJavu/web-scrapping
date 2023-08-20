1fe21718579af71a59d37edcfe2c4a6dedc06002
CaamVilvactDJavu <caam.vilvact11@gmail.com>
2023-08-20 18:30:20 +0700

    Login is Succesfully

--- backend/scraper2.py
+++ backend/scraper2.py
@@ -1,7 +1,6 @@
 from flask import Flask, jsonify
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from bs4 import BeautifulSoup
 from flask_cors import CORS
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -11,71 +10,53 @@ app = Flask(__name__)
 CORS(app)
 
 
-@app.route('/sensors2', methods=['GET', 'POST'])
+@app.route('/sensors2', methods=['GET'])
 def sensors():
-    url = "https://simora.bmkg.go.id/simora/simora_upt/status_acc2"
     login_url = "https://simora.bmkg.go.id/simora/web/login_page"
+
+    username = 'stageof.padangpanjang'
+    password = '12345678'
 
     chrome_options = Options()
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--disable-gpu")
     chrome_options.add_argument("--no-sandbox")
 
-    all_table_data = []
-
     with webdriver.Chrome(options=chrome_options) as driver:
-        # Visit login page and perform login
         driver.get(login_url)
 
-        # Assuming the username field has an id 'username' and password field has an id 'password'
-        # and the login button has an id 'loginBtn'. Adjust these selectors as per actual elements on the webpage.
-        username_field = driver.find_element(
-            By.XPATH, "/html/body/div/div/div/div/div[1]/div/form/div[1]/div/input")
-        password_field = driver.find_element(
-            By.XPATH, "/html/body/div/div/div/div/div[1]/div/form/div[2]/div/input")
-        login_button = driver.find_element(
-            By.XPATH, "/html/body/div/div/div/div/div[1]/div/form/div[3]/input")
-
-        # Set your actual username and password here
-        username_field.send_keys("")
-        password_field.send_keys("")
-        login_button.click()
-
-        # Wait for some time to ensure login is complete
-        WebDriverWait(driver, 10).until(EC.url_changes(login_url))
-
-        # Once logged in, visit the desired page
-        driver.get(url)
-
-        WebDriverWait(driver, 60).until_not(
-            EC.text_to_be_present_in_element(
-                (By.XPATH, "//table[@id='example']//td[contains(text(), 'Loading...')]"), "Loading...")
-        )
-
         try:
-            table_element = driver.find_element(By.XPATH, "//*[@id='example']")
+            # Use explicit waits
+            wait = WebDriverWait(driver, 15)
+
+            # Login process
+            username_input = wait.until(
+                EC.presence_of_element_located((By.ID, 'exampleInputEmail')))
+            password_input = wait.until(
+                EC.presence_of_element_located((By.ID, 'exampleInputPassword')))
+            login_button = wait.until(EC.presence_of_element_located(
+                (By.XPATH, '//input[@name="login"]')))
+
+            username_input.send_keys(username)
+            password_input.send_keys(password)
+            login_button.click()
+
+            print("Logged in successfully!")
+            print("URL after login:", driver.current_url)
+
+            # Click on the sidebar item after logging in
+            print("Waiting for sidebar element...")
+            sidebar_element = wait.until(EC.element_to_be_clickable(
+                (By.XPATH, '//*[@id="sidebar"]/ul/li[2]/a')))
+            sidebar_element.click()
+            print("Clicked on the sidebar anchor")
+
+            return jsonify({"message": "Logged in and clicked the sidebar successfully!"})
+
         except Exception as e:
-            print("Error finding table:", e)
-            return jsonify({"error": "Table not found"}), 404
-
-        table_html = table_element.get_attribute('outerHTML')
-        soup = BeautifulSoup(table_html, 'html.parser')
-        table = soup.find("table")
-
-        headers = [header.text for header in table.find(
-            "thead").find_all("th")]
-        tbody = table.find("tbody")
-
-        rows = tbody.find_all("tr")
-        for row in rows:
-            columns = row.find_all("td")
-            row_data = {}
-            for header, column in zip(headers, columns):
-                row_data[header] = column.text.strip()
-            all_table_data.append(row_data)
-
-    return jsonify(all_table_data)
+            print("Error:", e)
+            return jsonify({"error": f"An error occurred: {str(e)}"}), 500
 
 
 if __name__ == "__main__":
-    app.run(port=3000, debug=True)
+    app.run(port=3001, debug=True)
--- frontend/src/Sensor2.jsx
+++ frontend/src/Sensor2.jsx
@@ -1,21 +1,24 @@
 import React, { useState, useEffect } from 'react';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
+import Navbar from "./components/Navbar"
+import Footer from "./components/Footer"
 
-
-function Sensor2() {
-    const [rows, setRows] = useState([]);
+function Sensors2() {
+    const [data, setData] = useState(null);
     const [error, setError] = useState(null);
 
     useEffect(() => {
         async function fetchData() {
             try {
-                const response = await fetch("http://localhost:3000/sensors2");
-                const result = await response.json();
-                setRows(result);  // reverse the rows here
+                const response = await fetch("http://localhost:3001/sensors2");
+                if (response.ok) {
+                    const result = await response.json();
+                    setData(result.message);
+                } else {
+                    const result = await response.json();
+                    setError(result.error);
+                }
             } catch (error) {
-                console.error("Error fetching data:", error);
-                setError("Error loading data.");
+                setError("Error fetching data.");
             }
         }
         fetchData();
@@ -24,40 +27,19 @@ function Sensor2() {
     return (
         <>
             <Navbar />
-            <h1 className="text-4xl mb-6 font-bold text-center">Sensors</h1>
-            <div className="flex flex-col min-h-screen overflow-x-auto">
-                <div className='flex-grow overflow-x-auto'>
-                    {error ? (
-                        <p>{error}</p>
-                    ) : rows.length === 0 ? (
-                        <p>Loading...</p>
-                    ) : (
-                        <table className="table w-full">
-                            <thead>
-                                <tr>
-                                    <th>No.</th>  {/* Keep row number header as is */}
-                                    {[...Object.keys(rows[0])].reverse().map((header) => (   // Reverse the headers
-                                        <th key={header}>{header}</th>
-                                    ))}
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {rows.map((row, rowIndex) => (
-                                    <tr key={rowIndex}>
-                                        <td>{rowIndex + 1}</td>  {/* Keep row number as is */}
-                                        {[...Object.values(row)].reverse().map((value, valueIndex) => (  // Reverse the row values
-                                            <td key={valueIndex}>{value}</td>
-                                        ))}
-                                    </tr>
-                                ))}
-                            </tbody>
-                        </table>
-                    )}
-                </div>
+            <div>
+                <h1>Flask Backend Response</h1>
+                {error ? (
+                    <div>Error: {error}</div>
+                ) : !data ? (
+                    <div>Loading...</div>
+                ) : (
+                    <div>{data}</div>
+                )}
             </div>
             <Footer />
         </>
     );
 }
 
-export default Sensor2;
+export default Sensors2;
